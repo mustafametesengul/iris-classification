bffa97b9e0261b14684081dca2592dd06428aba1
Mete <mustafametesengul@gmail.com>
2020-03-16 23:29:49 +0000

    Design update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,7 +6,6 @@
       height=50
       color="grey darken-4"
     >
-      <img class="mr-3" :src="require('@/assets/flower.png')" height="30"/>
       <v-toolbar-title>Iris Classification</v-toolbar-title>
     </v-app-bar>
 
